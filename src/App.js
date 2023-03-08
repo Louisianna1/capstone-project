a8c508ce5ba863dd1987e05209d8d35da18ce26a
Louise Yufit <louisianna_1@hotmail.com>
2023-03-08 12:06:37 +0000

    Updated heading text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Homepage</h1>
+      <h1>Homepage v2</h1>
     </div>
   );
 }
